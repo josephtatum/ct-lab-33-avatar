a29e4e57d9953ef50a03a17dfe0a9fc84bbf4709
Joseph Tatum <josephtatum@gmail.com>
2020-02-05 14:49:22 -0800

    added header comonent

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,13 @@ import {
 import { Characters } from '../containers/Characters';
 import { Pagination } from './Pagination';
 import '../styles.css';
+import { Header } from './Header';
 
 export default function App() {
 
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/" component={Characters} />
       </Switch>
